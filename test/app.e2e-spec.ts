6d75ad751df831ad740d5f9fad726c71f5b655cb
AndersonAndrad <anderson_andrade_@outlook.com>
2023-05-03 21:44:01 -0300

    test: add test to fail to test CI

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,9 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
+
+import { Test, TestingModule } from '@nestjs/testing';
+
 import { AppModule } from './../src/app.module';
+import { INestApplication } from '@nestjs/common';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -20,5 +22,9 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('test to failed', () => {
+    return request(app.getHttpServer()).get('/').expect(400).expect('falided');
   });
 });
